67e1542b6e6a536ef71cec0bfcbb9680dd4132f3
Nicolás Muñoz <nicolas.munoz.r@usach.cl>
2024-12-11 21:11:16 -0300

    Ac

--- simulacionCredito-service/src/main/java/simulacionCreditoservice/simulacionCredito_service/WebConfig/WebConfig.java
+++ simulacionCredito-service/src/main/java/simulacionCreditoservice/simulacionCredito_service/WebConfig/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Especifica el origen permitido
+                .allowedOrigins("http://localhost:5173") // Especifica el origen permitido
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
